e115e996831c43216beccd96ca0337478f425e15
zawadzamil <contact.zawadzamil@gmail.com>
2023-12-01 21:27:18 +0600

    cloud added

--- src/assets/asset.js
+++ src/assets/asset.js
@@ -1,11 +1,13 @@
 import searchIcon from '../assets/icons/Search.png';
 import cartIcon from '../assets/icons/cart.png';
 import cloud from './images/cloud.png';
+import floatingCloud from './images/floating_cloud.png'
 
 const assets = {
     searchIcon,
     cartIcon,
-    cloud
+    cloud,
+    floatingCloud
 }
 
 export default assets;
--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -7,6 +7,7 @@ export default function Hero() {
   return (
     <Fragment>
       <div className=" hero w-full h-[720px]  flex flex-col items-center ">
+        {/* Text Section */}
         <div className="w-full text-center justify-center ps-[10rem] pe-[3rem] pt-[182px] flex gap-0 relative">
           <div className="heading ps-4">
             <span className="text-title_green text-5xl font-bold font-lato">
@@ -20,16 +21,22 @@ export default function Hero() {
             <img src={assets.cloud} alt="" className="absolute top-0" />
           </div>
         </div>
+        {/* Subtitle Section */}
         <div className="subtitle w-full text-center px-[7rem] font-lato text-[24px] capitalize leading-normal">
           <p>
             Sharpen your skills and elevate your success through realistic
             online model tests.
           </p>
         </div>
-
+        {/* Button Section */}
         <div className="buttons mt-16 flex justify-center items-center gap-4">
-          <GreenButton title="View Model Tests" py = "xx"></GreenButton>
+          <GreenButton title="View Model Tests" py="xx"></GreenButton>
           <WhiteButton title="Explore Categories" py="xx"></WhiteButton>
+        </div>
+
+        {/* Cloud Section */}
+        <div className="cloud relative flex-item ml-auto">
+            <img src={assets.floatingCloud} alt="" className="w-[180px] h-[53px] mr-28 mt-20"/>
         </div>
       </div>
     </Fragment>
