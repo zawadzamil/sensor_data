ead301ee543fd3257d3a94b184dfca27e43cecb4
zawadzamil <contact.zawadzamil@gmail.com>
2024-03-30 20:52:28 +0600

    checkout cart section completed

--- src/components/buttons/GreenButton.jsx
+++ src/components/buttons/GreenButton.jsx
@@ -1,13 +1,14 @@
 /* eslint-disable react/prop-types */
 
+import { twMerge } from "tailwind-merge";
 
-export default function GreenButton({ children, ...rest }) {
+
+export default function GreenButton({ children, className }) {
 
 
   return (
     <button
-      className={`leading-24 rounded h-[3rem] bg-title_green px-[24px] py-[8px] font-600 text-16p text-white tracking-[0.5px]`}
-      {...rest}
+      className={twMerge("leading-24 rounded h-[3rem] bg-title_green px-[24px] py-[8px] font-600 text-16p text-white tracking-[0.5px]", className)}
     >
       {children}
     </button>
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,6 +1,8 @@
 import { Fragment } from "react";
 import CartItem from "../components/cards/CartItem";
 
+import GreenButton from "../components/buttons/GreenButton";
+
 export default function Checkout() {
     return (
         <Fragment>
@@ -11,7 +13,7 @@ export default function Checkout() {
                     <div className="list mt-6">
                         <h3 className="base_bold px-2 py-4 border-b-2 w-fit">Cart items (2)</h3>
 
-                        <div className="cart_section grid  grid-cols-4 gap-28">
+                        <div className="cart_section grid  grid-cols-4 lg:gap-8 gap-28  ">
 
                             <div className="cart col-span-3">
                                 {/* Cart Items */}
@@ -19,8 +21,17 @@ export default function Checkout() {
                                 <CartItem></CartItem>
                             </div>
 
-                            <div className="order_details">
-                                Order Details
+                            {/* Order Summary */}
+                            <div className="order_summary p-6 bg-natural_bg_green mt-6 h-fit">
+                                <h3 className="small_semibold border-b-2">Order Summary</h3>
+                                <div className="pricing mt-9 flex justify-between">
+                                    <h3 className="base_normal text-natural">Total :</h3>
+                                    <h3 className="base_normal text-natural">$ 690.00</h3>
+
+                                </div>
+                                <div className="mt-16">
+                                    <GreenButton className="h-fit text-md font-400">Proceed to Checkout</GreenButton>
+                                </div>
                             </div>
                         </div>
                     </div>
